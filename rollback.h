bbb9edc87bac3f00c51b07d54e959bed5a3f4d8c
chaitanya-dama <110765443+chaitanya-dama@users.noreply.github.com>
2023-11-08 18:35:55 +0530

    Adding status and rollback utility files

---
+++ rollback.h
@@ -0,0 +1,347 @@
+#include <iostream>
+#include <dirent.h>
+#include <filesystem>
+#include <string>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fstream>
+#include <stdio.h>
+#include <iostream>
+#include <dirent.h>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <utility>
+// using namespace std;
+
+namespace rollback
+{
+
+    using namespace std;
+    namespace fs = std::filesystem;
+
+    string vcspath = "./.vcs";
+    string version_path = vcspath;
+
+
+    /*  */
+    bool check_if_dir_struct_exists(string check_path, string version_path)
+    {
+
+        string cwd = fs::current_path();
+        // cout<<"Check "<<check_path<<endl;
+        // char resolved[3000];
+        // realpath(check_path.c_str(), resolved);
+        string abs_path = check_path;
+        // cout << "Absolute Path: " << abs_path << endl;
+        vector<string> directories_in_path;
+        string temp;
+        // version_path=version_path.substr(0,version_path.size()-1);
+
+        if (!fs::exists(check_path))
+        {
+            return false;
+        }
+
+        for (int i = 0; i < (int)abs_path.size(); i++)
+        {
+            if (abs_path[i] != '/')
+            {
+                temp += abs_path[i];
+            }
+            else
+            {
+                directories_in_path.push_back(temp);
+                temp = "";
+            }
+        }
+        directories_in_path.push_back(temp);
+        string temp_path = version_path;
+        string rm_path = ".";
+        // cout << "Version path : " << version_path << endl;
+        for (auto i : directories_in_path)
+        {
+            if (i == ".")
+                continue;
+
+            temp_path = temp_path + "/" + i;
+            rm_path = rm_path + "/" + i;
+
+            if (fs::exists(temp_path))
+            {
+                // cout << "This exist in vcs : " << temp_path << endl;
+                // continue;
+            }
+            else
+            {
+                string remove_command = "rm -r " + rm_path;
+                system(remove_command.c_str());
+                // cout << remove_command << endl;
+                // break;
+                return true;
+            }
+        }
+        // cout << "End of removee ir" << endl;
+        return false;
+    }
+    
+    
+    /* create a directory with rwx permissions to ugo at the specified path */
+    int createdir(string path1)
+    {
+        int check;
+        check = mkdir(path1.c_str(), 0777);
+        if (check == 0)
+            return 1;
+        else
+            return -1;
+    }
+
+
+    /* when given a path to directory, create a similar structure in cwd as in the given path */
+    void create_dir_structure(string check_path)
+    {
+        string cwd = fs::current_path();
+        // cout << check_path << endl;
+        // char resolved[3000];
+        // realpath(check_path.c_str(), resolved);
+        string abs_path = check_path;
+        // cout << "Absolute Path: " << abs_path << endl;
+        vector<string> directories_in_path;
+        string temp;
+        for (int i = 0; i < (int)abs_path.size(); i++)
+        {
+            if (abs_path[i] != '/')
+            {
+                temp += abs_path[i];
+            }
+            else
+            {
+                directories_in_path.push_back(temp);
+                temp = "";
+            }
+        }
+        string temp_path = cwd;
+        // cout << cwd << endl;
+        for (auto i : directories_in_path)
+        {
+            // cout << "Here " << i << endl;
+            temp_path = temp_path + "/" + i;
+            if (fs::exists(temp_path))
+            {
+                continue;
+            }
+            else
+            {
+                string create_command = "mkdir " + temp_path;
+                system(create_command.c_str());
+            }
+        }
+    }
+
+
+    /* Get all files recursively in the path/dirName directory and store it in the given vector */
+    void getFileRecursive(vector<string> &st, string path, string dirName, int mode)
+    {
+
+        DIR *dir;
+        struct dirent *sd;
+        // cout << "Inside dir: ";
+        string fullPath = path + dirName;
+        // cout << fullPath << endl;
+        // string version_path = vcspath + "/" + fullPath;
+
+        // cout << fullPath << endl;
+        dir = opendir(fullPath.c_str());
+        if (fs::is_empty(fullPath) && mode)
+        {
+            if (check_if_dir_struct_exists(fullPath, version_path))
+            {
+                // cout << fullPath << endl;
+                // cout << "True" << endl;
+                return;
+            }
+        }
+        // sd = readdir(dir);
+        while ((sd = readdir(dir)) != NULL)
+        {
+            string currFile = sd->d_name;
+            if (currFile == ".." || currFile == "." || currFile == ".vcs" || currFile == "add.h" || currFile == "commit.h" || currFile == "status.h" || currFile == "diff.h" || currFile == "vcs" || currFile == ".git" || currFile == ".vscode" || currFile == "main.cpp" || currFile == "rollback.h")
+                continue;
+            else
+            {
+                struct stat sfile;
+                currFile = path + dirName + "/" + sd->d_name;
+                stat(currFile.c_str(), &sfile);
+                // cout << currFile << endl;
+
+                if (fs::is_directory(currFile.c_str()))
+                {
+                    // cout << "recur " << dirName + "/" + sd->d_name << endl;
+                    getFileRecursive(st, path, dirName + "/" + sd->d_name, mode);
+                }
+                else
+                    st.push_back(dirName + "/" + sd->d_name);
+            }
+        }
+        // cout << "going out\n";
+    }
+
+
+    /* go to the previous commit version */
+    void go_to_previous(string version_path, string curr_path)
+    {
+        DIR *dir1, *dir2, *dir;
+        struct dirent *sd;
+
+        // cout << "Version Path : " << version_path << endl;
+        // cout << "Current path : " << curr_path << endl;
+        dir1 = opendir(version_path.c_str());
+        dir2 = opendir(curr_path.c_str());
+
+        if (dir1 == NULL || dir2 == NULL)
+        {
+            // cout << "Here" << endl;
+            cout << "'.vcs' folder corrupted.\n";
+            return;
+        }
+
+        vector<string> old, latest; // stores path of all the files / directories
+        dir = dir2;
+        while ((sd = readdir(dir)) != NULL)
+        {
+            string currFile = sd->d_name;
+            // string fileDetails = getFileDetails(dir_to_search + '/'+sd->d_name);
+            if (currFile == ".." || currFile == "." || currFile == ".vcs" || currFile == "add.h" || currFile == "commit.h" || currFile == "status.h" || currFile == "diff.h" || currFile == "vcs" || currFile == ".git" || currFile == ".vscode" || currFile == "main.cpp" || currFile == "rollback.h")
+                continue;
+            else
+            {
+                struct stat sfile;
+                currFile = curr_path + sd->d_name;
+                stat(currFile.c_str(), &sfile);
+
+                if (fs::is_directory(currFile.c_str()))
+                    getFileRecursive(latest, curr_path, sd->d_name, 1);
+                else
+                    latest.push_back(sd->d_name);
+            }
+            // cout << "latest " << sd->d_name << endl;
+        }
+
+        dir = dir1;
+        while ((sd = readdir(dir)) != NULL)
+        {
+            string currFile = sd->d_name;
+            // string fileDetails = getFileDetails(dir_to_search + '/'+sd->d_name);
+            if (currFile == ".." || currFile == "." || currFile == ".vcs" || currFile == "add.h" || currFile == "commit.h" || currFile == "status.h" || currFile == "diff.h" || currFile == "vcs" || currFile == ".git" || currFile == ".vscode" || currFile == "main.cpp")
+                continue;
+            else
+            {
+                struct stat sfile;
+                currFile = version_path + currFile;
+                stat(currFile.c_str(), &sfile);
+
+                if (fs::is_directory(currFile.c_str()))
+                {
+                    // cout << "Before The Old Dirctory Name is : "<< sd->d_name << endl;
+                    getFileRecursive(old, version_path, sd->d_name, 0);
+                    // cout << "After The Old Dirctory Name is : "<<sd->d_name << endl;
+                }
+
+                else
+                    old.push_back(sd->d_name);
+            }
+            // cout << "old " << sd->d_name << endl;
+        }
+
+        // for (auto it : old)
+        // {
+        //     cout << "old : " << it << endl;
+        // }
+
+        // for (auto it : latest)
+        // {
+        //     cout << "current : " << it << endl;
+        // }
+
+        for (auto it : latest)
+        {
+            string add_patch = it + ".patch";
+
+            auto prev_itr = find(old.begin(), old.end(), add_patch);
+            if (prev_itr != old.end())
+            {
+                string command = "patch " + it + " " + version_path + add_patch;
+                system(command.c_str());
+                old.erase(prev_itr);
+            }
+
+            else
+            {
+                string command = "rm " + it;
+
+                // cout<<"Here Above"<<endl;
+                if (check_if_dir_struct_exists(it, version_path))
+                {
+                    // cout << "HEre : " << command << endl;
+                    // system(command.c_str());
+                }
+
+                // cout<<"Here Above"<<endl;
+                // ifstream ud(it, ios::ate | ios::binary);
+
+                // cout<<"Here"<<endl;
+                // if (ud.good())
+                // {
+                // cout << command << endl;
+
+                // }
+                // else
+                // {
+                //     cout << "Ignoring the file presented in deleted folder " << endl;
+                // }
+            }
+        }
+
+        for (auto it : old)
+        {
+            create_dir_structure(it);
+            string command = "cp " + version_path + it + " " + "./" + it;
+            system(command.c_str());
+        }
+    }
+
+
+    /* go back to the specified version number */
+    void rollback(string version_no, string curr_path, int total_commits)
+    {
+        // string version_path = "./.vcs/" + version_no + "/";
+        // version_path = vcspath + "/" + version_no + "/";
+
+        int count = total_commits - stoi(version_no) - 1; // Number of times to rollback
+        int i = 1;
+        while (count > 0)
+        {
+
+            cout << "Rolling Backing to : " << total_commits - 1 - i << endl;
+            // cout<<"----------------------------------------------------------------------------------"<<endl;
+            version_path = vcspath + "/" + to_string(total_commits - 1 - i) + "/";
+            go_to_previous(version_path, curr_path);
+            count--;
+            i++;
+            // int vno = stoi(version_no);
+            // vno--;
+            // version_no = to_string(vno);
+        }
+
+        return;
+    }
+
+}
---
+++ status.h
@@ -0,0 +1,328 @@
+#include <iostream>
+#include <dirent.h>
+#include <filesystem>
+#include <string>
+#include <string.h>
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fstream>
+#include <stdio.h>
+#include <iostream>
+#include <dirent.h>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <utility>
+#include <openssl/sha.h>
+
+namespace status
+{
+    using namespace std;
+    namespace fs = std::filesystem;
+    string vcspath1 = "./.vcs/tracked_current.txt";
+    string vcspath2 = "./.vcs/tracked_history.txt";
+    map<string, string> previousSHA;            // <filename, sha1_value> of files in staging area
+    map<string, string> currSHA;                // <filename, sha1_value> of files in cwd area
+    map<string, string> currTrackedSHA;
+
+
+    /* Given the file name path, just get the file name */
+    string getFileName(string fileName)
+    {
+        string cwd = fs::current_path();
+        // cout << cwd;
+        return fileName.substr(cwd.size() + 1, fileName.size());
+    }
+
+
+    /* Read the file in 512KB chunks and find the SHA-1 value for the entire file ;
+       Now store : fileLoc + "$" + fullFileSHA1 + "\n"   in   ./.vcs/tracked_current.txt file
+       Seems like tracked_current.txt file in .vcs directory is simulating the staging area
+    */
+    void getCurrentFileSHA(string fileLoc)
+    {
+
+        unsigned char completeFileSHA[SHA_DIGEST_LENGTH];
+        FILE *inFile = fopen(fileLoc.c_str(), "rb");
+        SHA_CTX shaContext;
+        int bytes;
+        unsigned char data[524288];
+
+        if (inFile == NULL)
+        {
+            cout << "Cannot open " << fileLoc << endl;
+            return;
+        }
+
+        // vector<string> chunkSHA1;
+        SHA1_Init(&shaContext);
+        while ((bytes = fread(data, 1, 524288, inFile)) != 0)
+        {
+            SHA1_Update(&shaContext, data, bytes);
+        }
+        SHA1_Final(completeFileSHA, &shaContext);
+        fclose(inFile);
+        stringstream ss;
+        for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
+        {
+            ss << hex << setw(2) << setfill('0') << (int)completeFileSHA[i];
+        }
+        string fullFileSHA1 = ss.str();
+        // cout << fileLoc << " sha is " << fullFileSHA1 << endl;
+        currSHA[fileLoc] = fullFileSHA1;
+    }
+
+
+    // get previous files SHA from version.info
+    void getPrevFilesSHA()
+    {
+
+        string line;
+
+        ifstream myfile;
+        myfile.open(vcspath2);
+
+        if (!myfile.is_open())
+        {
+
+            perror("Error open");
+            return;
+        }
+
+        // get files SHA from tracked_history
+        while (getline(myfile, line))
+        {
+            string fileData = line;
+            int pos = fileData.find("$");
+
+            string fileLoc = fileData.substr(0, pos);
+            string fileSHA = fileData.substr(pos + 1);
+            // cout << fileLoc << " " << fileSHA << endl;
+            char resolved[2000];
+            realpath(fileLoc.c_str(), resolved);
+            string abs_path = string(resolved);
+            previousSHA[abs_path] = fileSHA;
+        }
+        myfile.close();
+
+        ifstream myfile1;
+        // cout << "going for path1 ?" << endl;
+        myfile1.open(vcspath1);
+        if (!myfile1.is_open())
+        {
+            perror("Error open");
+            return;
+        }
+        // get files SHA from tracked_current
+        while (getline(myfile1, line))
+        {
+            // cout << "COMING INSIDE";
+
+            string fileData = line;
+            int pos = fileData.find("$");
+            string fileLoc = fileData.substr(0, pos);
+            string fileSHA = fileData.substr(pos + 1);
+            char resolved[2000];
+            realpath(fileLoc.c_str(), resolved);
+            string abs_path = string(resolved);
+            // cout << fileLoc << " " << fileSHA << endl;
+            previousSHA[abs_path] = fileSHA;
+            // cout << "Adding SHA to " << abs_path << endl;
+            currTrackedSHA[abs_path] = fileSHA;
+        }
+
+        myfile1.close();
+    }
+
+
+    /* Traverse all files in cwd recursively and store < curr_file, it's_sha1_value >in the corresponding map */
+    void traverseFiles(string cwd)
+    {
+        // cout << "+++++++++++++++++++++++++++++++++++++++" << endl;
+        // get current files SHA
+        DIR *folder = opendir(cwd.c_str());
+        if (folder == NULL)
+        {
+            perror("Error open");
+            return;
+        }
+        // reading files in a directory
+        struct dirent *file = readdir(folder);
+        while (file != NULL)
+        {
+            if (strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0 || strcmp(file->d_name, ".vcs") == 0 || strcmp(file->d_name, ".git") == 0 || strcmp(file->d_name, ".vscode") == 0 || strcmp(file->d_name, "main.cpp") == 0 || strcmp(file->d_name, "add.h") == 0 || strcmp(file->d_name, "commit.h") == 0 || strcmp(file->d_name, "diff.h") == 0 || strcmp(file->d_name, "status.h") == 0 || strcmp(file->d_name, "rollback.h") == 0 || strcmp(file->d_name, "vcs") == 0){
+
+            }
+            else{
+                string filepath = cwd + "/" + file->d_name;
+                string new_path = "./.vcs/0/" + string(file->d_name);
+                if (file->d_type == DT_DIR)
+                {
+                    // cout << "Directory" << filepath << endl;
+                    traverseFiles(filepath);
+                }
+                else
+                {
+                    getCurrentFileSHA(filepath);
+                }
+            }
+            file = readdir(folder);
+        }
+        closedir(folder);
+    }
+
+
+    /* Compare prev sha map and curr sha map and think of all cases and figure out untracked, modofied, deleted, staged files */
+    void git_status(map<string, string> &previous_sha, map<string, string> &current_sha)
+    {
+        string un_tracked = "Untracked Files:\n";
+        string deleted_file = "Deleted Files:\n";
+        string modified_file = "Modified Files:\n";
+        string tracked_file = "Staged Files:\n";
+        string name_of_un = "";
+        string name_of_del = "";
+        string name_of_mod = "";
+        string name_of_track = "";
+
+        if (previous_sha.size() != 0)
+        {
+            // cout << "Inside" << endl;
+            for (auto i : previous_sha)
+            {
+                // cout << "file path is:--> " << i.first << endl;
+                if (current_sha.count(i.first) > 0)
+                {
+                    if (current_sha[i.first] == previous_sha[i.first])
+                    {
+                        // cout << "Here--->>" << i.first << endl;
+                        //  name_of_track += i.first + "\n";
+                    }
+                    else                                            //this file is different from prev version => modified
+                    {
+                        name_of_mod += "\t modified:   " + (getFileName(i.first));
+                        name_of_mod += "\n";
+                        // cout << "Modified file-->>" << name_of_mod;
+                    }
+                }   
+                else                                                //this file is not in cwd => it is deleted
+                {
+                    name_of_del += "\t deleted:   " + (getFileName(i.first));
+                    name_of_del += "\n";
+                }
+            }
+        }
+
+        for (auto i : current_sha)
+        {
+            if (previous_sha.count(i.first) == 0)                  //this file was not tracked previously
+            {
+                name_of_un += "\t untracked:   " + (getFileName(i.first));
+                name_of_un += "\n";
+            }
+        }
+
+        for (auto i : currTrackedSHA)
+        {
+
+            name_of_track += "\t staged:   " + (getFileName(i.first));
+            name_of_track += "\n";
+        }
+
+        string output = "";
+        if (name_of_del.size() == 0 && name_of_un.size() == 0 && name_of_track.size() == 0 && name_of_mod.size() == 0)
+        {
+            cout << "\033[0;36m";
+            cout << "Working directory clean, nothing to commit\n";
+            cout << "\033[0m";
+        }
+        else
+        {
+
+            cout << "\033[1;32m";
+            cout << "\033[4;32m";
+            cout << tracked_file<< endl;
+            cout << "\033[0m";
+
+            cout << "\033[0;32m";
+            cout << name_of_track<< endl;
+            cout << "\033[0m";
+
+            cout << "\033[1;36m";
+            cout << "\033[4;36m";
+            cout << un_tracked<< endl;
+            cout << "\033[0m";
+
+            cout << "\033[0;36m";
+            cout << name_of_un<< endl;
+            cout << "\033[0m";
+
+            cout << "\033[1;31m";
+            cout << "\033[4;31m";
+            cout << deleted_file<< endl;
+            cout << "\033[0m";
+
+            cout << "\033[0;31m";
+            cout << name_of_del<< endl;
+            cout << "\033[0m";
+
+            cout << "\033[1;33m";
+            cout << "\033[4;33m";
+            cout << modified_file<< endl;
+            cout << "\033[0m";
+
+            cout << "\033[0;33m";
+            cout << name_of_mod<< endl;
+            cout << "\033[0m";
+
+
+            // output += tracked_file;
+            // output += name_of_track;
+            // output += "\n";
+            // output += un_tracked;
+            // output += name_of_un;
+            // output += "\n";
+            // output += deleted_file;
+            // output += name_of_del;
+            // output += "\n";
+            // output += modified_file;
+            // output += name_of_mod;
+        }
+
+        // cout << output;
+    }
+
+    /* just like git status, it's implementation is the above function */
+    void vcsCmndStatus()
+    {
+        
+        getPrevFilesSHA();                          //compute prev version files SHA values and fill in the corresponding map
+        string cwd = fs::current_path();
+        traverseFiles(cwd);
+        // cout << "Current SHA vals" << endl;
+        // for(auto itr = currSHA.begin();itr != currSHA.end();itr++)
+        //     cout << itr->first << " " << itr->second << endl;
+
+        // cout << "Previous SHA vals" << endl;
+        // for(auto itr = previousSHA.begin();itr != previousSHA.end();itr++)
+        //     cout << itr->first << " " << itr->second << endl;
+
+        git_status(previousSHA, currSHA);
+    }
+
+}
+
+/*
+    Summary of implementation of status command : 
+    In the curr directory, when we do git status : 
+        1. Figure out all the files and directories recursively in the cwd, Find their current sha-1 values
+        2. Go to the staging area and figure out it's contents and their sha-1 values
+        3. Now compare both the prevSHA map and currSHA map and find out which files are modified, deleted, untracked, staged
+        4. You need to check different cases to do that.
+
+*/
